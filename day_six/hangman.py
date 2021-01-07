be20d3269a955423aa11817743bf2bae61bb7959
roblivesinottawa <tech.rob@icloud.com>
2021-01-07 13:07:31 -0500

    "hangman finished"

--- day_six/hangman.py
+++ day_six/hangman.py
@@ -1,22 +1,46 @@
-wordList = ['fuck', 'shit', 'ass', 'motherfucker', 'butthole']
-
 import random
-word = random.choice(wordList)
+from hangmanwords import wordlist
+from hangmanart import logo, stages
 
-# line test
-print(f"the solution is {word}")
-
-display = []
+word = random.choice(wordlist)
 wordlength = len(word)
+
+endgame = False
+lives = 6
+
+print(logo)
+
+# create blanks
+display = []
 for _ in range(wordlength):
   display += "_"
 
 
-guess = input("can you guess a letter? >>> ").lower()
+while not endgame:
+    guess = input("can you guess a letter? >>> ").lower()
 
-for position in range(wordlength):
-  letter = word[position]
-  if letter == guess:
-    display[position] = letter
+    if guess in display:
+        print(f"you've already guessed {guess}")
+    # check guessed letter
+    for position in range(wordlength):
+        letter = word[position]
+        # print(f"current position: {position}\n current letter: {letter}\n guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
-print(display)
+    if guess not in word:
+        print(f"you guessed {guess}, that's not in the word. you lose a life.")
+        lives -= 1
+        if lives == 0:
+            endgame = True
+            print("you lose.")
+
+    # join all the elements into a string
+    print(f"{' '.join(display)}")
+
+    # check if user got all the letters
+    if "_" not in display:
+        endgame = True
+        print("you win.")
+
+    print(stages[lives])
