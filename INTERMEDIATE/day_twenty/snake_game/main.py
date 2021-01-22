5a8c14887bc4c0bd07c69f8e8da5198bbe740460
roblivesinottawa <tech.rob@icloud.com>
2021-01-22 13:09:32 -0500

    "some updates"

---
+++ INTERMEDIATE/day_twenty/snake_game/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
+    
+        
+        
+
+
+
+screen.exitonclick()
